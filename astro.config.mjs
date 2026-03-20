81f96b5638cbcbff739eda99522f8896cdfba745
Mr. Audio Feedback <182364268+scomire@users.noreply.github.com>
2026-03-20 13:37:15 -0400

    add giscus comments, search, misc fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import { execSync } from 'child_process';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://mraudiofeedback.com',
+  integrations: [
+    sitemap(),
+    {
+      name: 'pagefind',
+      hooks: {
+        'astro:build:done': () => {
+          execSync('npx pagefind --site dist', { stdio: 'inherit' });
+        },
+      },
+    },
+  ],
 });
